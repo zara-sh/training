ed01e7e1426f79d8399d4209d11ca547edece4ce
zara <zara.regist@gmail.com>
2017-08-29 18:54:22 +0900

    adding randomQuotes

--- jquery/script.js
+++ jquery/script.js
@@ -1,3 +1,55 @@
+
+
+// email validation
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+    // alert( pattern.test(emailAddress) );
+    return pattern.test(emailAddress);
+};
+
 $(".btn").click(function(){
-	alert("hi");
+
+	var erroMessage = "";
+	var fieldsMissing = ""
+
+	if($("#exampleInputEmail1").val()== ""){
+
+		fieldsMissing += "<br> Email is missing!"
+	}
+	if($("#exampleInputTell").val()== ""){
+
+		fieldsMissing += "<br> phone number is missing!"
+	}
+
+	if($("#exampleInputPass").val()== ""){
+
+		fieldsMissing += "<br> passwords is missing!"
+	}
+
+	if($("#exampleInputPassConfirm").val()== ""){
+
+		fieldsMissing += "<br>confirm passwords is missing!"
+	}
+	if(fieldsMissing != ""){
+		erroMessage += "the following fields are missing" +
+		fieldsMissing;
+	}
+
+	if(isValidEmailAddress($("#exampleInputEmail1").val())== false){
+		erroMessage += "Your email addrss is not valid!"
+	}
+	
+	if ($.isNumeric($("#exampleInputTell").val())== false) {
+
+		erroMessage +="<p>Your phone number is not valid!</p>"
+	}
+	if($("#exampleInputPass").val()!= $("#exampleInputPassConfirm").val()){
+		erroMessage +="<p>Your passwords are not match!</p>"
+	}
+
+	if (erroMessage != "") {
+		$("#alert").html(erroMessage)
+	}
+	// alert(erroMessage)
+
 });
